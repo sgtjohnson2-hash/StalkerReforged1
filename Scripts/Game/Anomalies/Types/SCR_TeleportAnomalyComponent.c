768ba61b135783cde319f8b334f23cb07fba0037
Reddawn141 <sgt.johnson2@gmail.com>
2026-05-27 12:43:34 -0400

    Update SCR_TeleportAnomalyComponent.c

--- Scripts/Game/Anomalies/Types/SCR_TeleportAnomalyComponent.c
+++ Scripts/Game/Anomalies/Types/SCR_TeleportAnomalyComponent.c
@@ -6,7 +6,9 @@ class SCR_TeleportAnomalyComponent : SCR_AnomalyComponent
 {
 	override protected void OnEntityDamaged(IEntity ent)
 	{
-		// Teleports don't just damage, they move the entity
+		// Teleportation must be handled exclusively on server authority
+		if (!Replication.IsServer()) return;
+
 		vector newPos;
 		float randomX = Math.RandomFloat(-50, 50);
 		float randomZ = Math.RandomFloat(-50, 50);
@@ -14,11 +16,26 @@ class SCR_TeleportAnomalyComponent : SCR_AnomalyComponent
 		vector origin = ent.GetOrigin();
 		newPos = "0 0 0";
 		newPos[0] = origin[0] + randomX;
-		newPos[1] = origin[1] + 10; // Drop from sky
 		newPos[2] = origin[2] + randomZ;
+		newPos[1] = ent.GetWorld().GetSurfaceY(newPos[0], newPos[2]) + 10.0; // Teleport and drop 10m high
+
+		Physics phys = ent.GetPhysics();
+		if (phys)
+		{
+			// Disabling physics momentarily prevents coordinate lock glitches and rubberbanding
+			phys.SetActive(false);
+			ent.SetOrigin(newPos);
+			phys.SetActive(true);
+			
+			// Apply a smooth downward velocity to drop the player character
+			vector fallVel = "0 -2 0";
+			phys.SetVelocity(fallVel);
+		}
+		else
+		{
+			ent.SetOrigin(newPos);
+		}
 		
-		ent.SetOrigin(newPos);
-		
-		Print("Teleport anomaly triggered, teleported entity: " + ent.ToString());
+		Print("Server: Teleport anomaly triggered. Character translated to " + newPos.ToString());
 	}
 }
